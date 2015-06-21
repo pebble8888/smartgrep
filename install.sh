2a70d60e5a08b4d48cbc9f6b654b7b2156f11745
pebble8888 <pebble8888@gmail.com>
2015-06-21 13:45:13 +0900

    fix install.sh

--- install.sh
+++ install.sh
@@ -2,15 +2,15 @@
 # smartgrep installer
 # pebble8888@gmail.com
 
+MY_PATH=$HOME/bin
+
+if ! [ -d $MY_PATH ]; then
+  mkdir -p $MY_PATH
+  echo 
+fi
+
 RELATIVE_PATH="`dirname \"$0\"`"
 ABSOLUTE_PATH="`( cd \"$RELATIVE_PATH\" && pwd )`"
-
-USER_HOME=$(eval echo ~${SUDO_USER})
-MY_PATH=$USER_HOME/bin
-
-if [ -d $MY_PATH ]; then
-  mkdir -p $MY_PATH
-fi
 
 cd $MY_PATH
 ln -sf $ABSOLUTE_PATH/src/smartgrep smartgrep
